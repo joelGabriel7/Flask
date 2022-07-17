f3e5fa6931f9375fbd33a313150bb64ae2bb4f3b
joelGabriel7 <joelgerman671@gmail.com>
2022-07-17 01:30:55 -0500

    Saludando desde flask y pasando parametros a la url

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
+    app.logger.info(f'Entramos al path {request.path}')
     return 'Hello World desde Flask!'
 
+@app.route('/saludar/<nombre>')
+def saludar(nombre):
+    return f'Hola como estas {nombre}?'
 
 if __name__ == '__main__':
     app.run()
